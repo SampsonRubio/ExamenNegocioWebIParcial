d871f10b987e5f50c34f2ff24b659a1eca60acea
Camila Sampson <camilagsampr@gmail.com>
2020-06-09 15:48:19 -0600

    Base ingresar elemento y suncion cuota

--- controller/proyectoController.js
+++ controller/proyectoController.js
@@ -2,6 +2,10 @@ exports.home = (req, res, next) => {
   res.render("calculo");
 };
 
-exports.calculoA = (req, res, next) => {
+exports.calculo = (req, res, next) => {
   console.log(req.body);
+};
+
+exports.cuota = function (capital, interes, tiempo) {
+  return (capital * interes) / Math.pow((1 - (1 + interes), -tiempo));
 };
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const routes = express.Router();
 const proyectoController = require("../controller/proyectoController");
 module.exports = function () {
   routes.get("/", proyectoController.home);
-  routes.post("/amortizacion", proyectoController.calculoA);
+  routes.post("/amortizacion", proyectoController.calculo);
   return routes;
 };
-
